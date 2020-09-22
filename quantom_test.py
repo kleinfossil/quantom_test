0f665e0a41de2a7ec88712b57212d3ae44f604d5
kleinfossil <kleinfossil@googlemail.com>
2020-09-22 13:20:01 +0200

    Code Commented and quantom circuit method improved

--- quantom_test.py
+++ quantom_test.py
@@ -6,10 +6,12 @@ import numpy as np
 import pylatexenc
 
 
+# Prints out qiskit version and IBM Account Infos
 def qiskitversion():
-    print('Current qiskit Version: ',qiskit.__qiskit_version__)
-    print('Current IBM Account: ',IBMQ.load_account())
+    print('Current qiskit Version: ', qiskit.__qiskit_version__)
+    print('Current IBM Account: ', IBMQ.load_account())
 
+# The Quantom Circuit. Make your changes here.
 def quantomcircuit():
     qr = QuantumRegister(2)
     cr = ClassicalRegister(2)
@@ -19,16 +21,16 @@ def quantomcircuit():
     circuit.measure(qr, cr)
 
     circuit.draw(output='mpl')
-    # evenly sampled time at 200ms intervals
-    # t = np.arange(0., 5., 0.2)
     return circuit
 
-
-def quantomsimulator():
+# Executes the QASM Simulator
+def quantomsimulator(cir):
     simulator = Aer.get_backend('qasm_simulator')
+    result = execute(cir, backend=simulator).result()
+    plot_histogram(result.get_counts(cir))
     return simulator
 
-
+# Executes a real Quantom Computer
 def quantomcomputer(cir):
     IBMQ.load_account()
     provider = IBMQ.get_provider('ibm-q')
@@ -42,9 +44,9 @@ def quantomcomputer(cir):
 if __name__ == '__main__':
     # qiskitversion()
     cir = quantomcircuit()
-    result = execute(cir, backend=quantomsimulator()).result()
-    plot_histogram(result.get_counts(cir))
-    quantomcomputer(cir)
+
+    quantomsimulator(cir)
+    # quantomcomputer(cir)
 
     plt.show()
 
